c417ff4d49249b781b8e44bd288c5bc775b22723
Ashwin Joshi <ashwin.joshi032@gmail.com>
2025-09-01 20:47:51 +0530

    Add files via upload

---
+++ day1q1.c
@@ -0,0 +1,14 @@
+//write an program to input two numbers and display there sum
+
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter first number");
+    scanf("%d",&a);
+    printf("enter second number");
+    scanf("%d",&b);
+    c=a+b;
+    printf("the sum of %d and %d is %d",a,b,c);
+
+}
